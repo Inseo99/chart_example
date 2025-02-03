a1d266d4167bcb85d350a0d95e2b9c33b0cc6141
Inseo99 <jjj990626@naver.com>
2025-02-03 15:08:23 +0900

    postman, chatWriteMessage

--- src/main/java/com/koreait/exam/chat_example/Demo1Application.java
+++ src/main/java/com/koreait/exam/chat_example/Demo1Application.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.koreait.exam.chat_example;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/com/koreait/exam/chat_example/Demo1ApplicationTests.java
+++ src/test/java/com/koreait/exam/chat_example/Demo1ApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.demo;
+package com.koreait.exam.chat_example;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
