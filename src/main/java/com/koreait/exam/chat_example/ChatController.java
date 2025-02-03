d8862ebd54840e02456ef600fab985697ed9d349
Inseo99 <jjj990626@naver.com>
2025-02-03 15:47:12 +0900

    ajax 채팅4 메세지저장

--- src/main/java/com/koreait/exam/chat_example/ChatController.java
+++ src/main/java/com/koreait/exam/chat_example/ChatController.java
@@ -3,16 +3,29 @@ package com.koreait.exam.chat_example;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Controller
 @RequestMapping("/chat")
 public class ChatController {
 
-    ChatMessage message = new ChatMessage("김철수", "안녕");
+    private List<ChatMessage> chatMessages = new ArrayList<ChatMessage>();
+
+    public record writeChatMessageResponse(long id) {
+
+    }
 
     @GetMapping("/writeMessage")
     @ResponseBody
-    public RsData<ChatMessage> writeMessage() {
-        return new RsData<>("S-1", "메세지가 전달됨", message); // 보고서를 일관성있게 제출을 위해
+    public RsData<writeChatMessageResponse> writeMessage() {
+        ChatMessage message = new ChatMessage("김철수", "안녕");
+        chatMessages.add(message);
+        return new RsData<>(
+                "S-1",
+                "메세지가 전달됨",
+                new writeChatMessageResponse(message.getId())
+        ); // 보고서를 일관성있게 제출을 위해
     }
 
 }
